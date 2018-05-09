72328e72585953b7a842ad95927681fa321465dd
Tatiana Wu <twu99@stanford.edu>
2018-05-08 17:26:15 -0700

    Lab 10 :)

--- quadratic.py
+++ quadratic.py
@@ -6,19 +6,42 @@ import sys
 #Enter values for a, b, and c in the command line
 #e.g. run: >python quadratic.py 1 2 -15
 def main():
-    a = sys.argv[1]
-    b = sys.argv[2]
-    c = sys.argv[3]
-    x1, x2 = find_roots(a, b, c)
+    # Invalid argument(s)
+    try:
+        a = float(sys.argv[1])
+        b = float(sys.argv[2])
+        c = float(sys.argv[3])
+    except:
+        print("Invalid argument(s)")
+        return
+
+    try:
+        x1, x2 = find_roots(a, b, c)
+    except:
+        return
+    
     print ("This is x1: %f" %x1)
     print ("This is x2: %f" %x2)
 
 
 def find_roots(a,b,c):
-    mid = b^2 - 4*a*c
-    sqrt_mid = mid^(1/2)
-    x1 = (-b + sqrt_mid)/2*a
-    x2 = (-b - sqrt_mid)/2*a
+    mid = b**2 - 4*a*c
+
+    # Imaginary roots
+    try:
+        sqrt_mid = mid**(0.5)
+    except:
+        print("Imaginary solution(s)")
+        return
+
+    # Zero leading coefficient
+    try:
+        x1 = (-b + sqrt_mid)/(2*a)
+        x2 = (-b - sqrt_mid)/(2*a)
+    except:
+        print("Not a quadratic function: a = 0")
+        return
+
     return x1, x2
 
 
